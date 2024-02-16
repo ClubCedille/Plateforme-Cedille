50e17a93b60c90d8dc3f2eee26e45b2702089616
Simon-Boyer <si.boyer@hotmail.ca>
2024-02-15 20:26:03 -0500

    create philippe account

--- terraform/users.tf
+++ terraform/users.tf
@@ -12,3 +12,17 @@ module "francis" {
   cluster_role = "None"
   cluster_repo = var.platform_repo
 }
+
+module "francis" {
+  source          = "./modules/user"
+  github_email    = "philippelamy12@gmail.com"
+  github_username = "lamiphil"
+  github_role     = "member"
+  teams = [
+    { teamName = "members", teamRole = "member" },
+    { teamName = "sre", teamRole = "member" }
+  ]
+  cluster_name = var.cluster_name
+  cluster_role = "Read"
+  cluster_repo = var.platform_repo
+}
