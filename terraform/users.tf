5573baac0e1cdd6fe5965b48c9df841fcc3452ba
svc-cedille-user <svc-cedille-user@users.noreply.github.com>
2024-03-29 21:49:00 +0000

    [create-pull-request] automated change

--- terraform/users.tf
+++ terraform/users.tf
@@ -40,3 +40,17 @@ module "cedille-test" {
   cluster_role = "Reader"
   cluster_repo = var.platform_repo
 }
+module "sheli06" {
+  source          = "./modules/user"
+  github_email    = "saimahelioui@gmail.com"
+  github_username = "sheli06"
+  github_role     = "member"
+  teams = [
+    { teamName = "members", teamRole = "member" },
+    { teamName = "sre", teamRole = "member" },
+    { teamName = "webmasters", teamRole = "member" },
+  ]
+  cluster_name = var.cluster_name
+  cluster_role = "Reader"
+  cluster_repo = var.platform_repo
+}
