939a65b28b7f6d118a2d90515267e946d5da00a8
SonOfLope <SonOfLope@users.noreply.github.com>
2024-04-03 21:34:05 +0000

    [create-pull-request] automated change

---
+++ terraform/github/apps-repos/testcedille-grav.tf
@@ -0,0 +1,49 @@
+resource "github_repository" "repo_testcedille" {
+  name = "test.cedille.club"
+  auto_init = true
+  homepage_url = "https://test.cedille.club"
+  description = "Site web de testcedille"
+  has_downloads = true
+  has_issues = true
+  has_projects = true
+  has_wiki = true
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+  topics = [ "grav" ]
+  vulnerability_alerts = true
+  visibility = "public"
+}
+
+resource "github_repository_webhook" "webhook_testcedille" {
+  repository = github_repository.repo_testcedille.name
+
+  configuration {
+    url          = "https://test.cedille.club/_git_webhook"
+    content_type = "json"
+    insecure_ssl = false
+  }
+
+  active = true
+
+  events = ["push"]
+}
+
+resource "github_repository_dependabot_security_updates" "dependabot_testcedille" {
+  repository  = github_repository.repo_testcedille.name
+  enabled     = true
+}
+
+resource "github_repository_collaborators" "colaborators_testcedille" {
+  repository = github_repository.repo_testcedille.name
+
+  team {
+    permission = "admin"
+    team_id = "sre"
+  }
+}
