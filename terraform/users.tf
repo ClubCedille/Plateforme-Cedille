f2867672a92796858b467d069c8b2731c9ff0eaf
Jonathan Lopez <jonathanlopez@hotmail.ca>
2024-04-05 16:58:40 -0400

    Remove irelevant users
    + remove from sre since not needed

--- terraform/users.tf
+++ terraform/users.tf
@@ -13,41 +13,13 @@ module "francis" {
   cluster_repo = var.platform_repo
 }
 
-module "philippe" {
-  source          = "./modules/user"
-  github_email    = "philippelamy12@gmail.com"
-  github_username = "lamiphil"
-  github_role     = "member"
-  teams = [
-    { teamName = "members", teamRole = "member" },
-    { teamName = "sre", teamRole = "member" }
-  ]
-  cluster_name = var.cluster_name
-  cluster_role = "Reader"
-  cluster_repo = var.platform_repo
-}
-
-module "cedille-test" {
-  source          = "./modules/user"
-  github_email    = "cedille@etsmtl.net"
-  github_username = "svc-cedille-user"
-  github_role     = "member"
-  teams = [
-    { teamName = "members", teamRole = "member" },
-    { teamName = "sre", teamRole = "member" }
-  ]
-  cluster_name = var.cluster_name
-  cluster_role = "Reader"
-  cluster_repo = var.platform_repo
-}
-
 module "allaoua-rico" {
   source          = "./modules/user"
   github_email    = "allaoua.boudriou@gmail.com"
   github_username = "allaoua-rico"
   github_role     = "member"
   teams = [
-    { teamName = "members", teamRole = "member" }, { teamName = "sre", teamRole = "member" }, 
+    { teamName = "members", teamRole = "member" }, 
   ]
   cluster_name = var.cluster_name
   cluster_role = "Reader"
