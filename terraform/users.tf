66a8c1847fe7a1d565191ed4bb4c86acead7e5b0
SonOfLope <SonOfLope@users.noreply.github.com>
2024-09-30 16:34:43 +0000

    [create-pull-request] automated change

--- terraform/users.tf
+++ terraform/users.tf
@@ -134,3 +134,18 @@ module "AlexanderPan96" {
   cluster_repo = var.platform_repo
   netdata_role = "observer"
 }
+
+
+module "sonoflope" {
+  source          = "./modules/user"
+  github_email    = "jonathanlopez@hotmail.ca"
+  github_username = "sonoflope"
+  github_role     = "member"
+  teams = [
+    { teamName = "members", teamRole = "member" }, { teamName = "sre", teamRole = "member" }, 
+  ]
+  cluster_name = var.cluster_name
+  cluster_role = "Reader"
+  cluster_repo = var.platform_repo
+  netdata_role = "observer"
+}
