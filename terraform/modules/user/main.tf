268fa2123df00cb4c25d37c61162e4ce895290b4
Jonathan Lopez <jonathanlopez@hotmail.ca>
2025-07-04 19:23:15 -0400

    Update main.tf

--- terraform/modules/user/main.tf
+++ terraform/modules/user/main.tf
@@ -37,6 +37,6 @@ resource "github_repository_file" "omni_acl" {
 # Add user to netdata
 resource "netdata_space_member" "cedille_membership" {
   email    = var.github_email
-  space_id = "de0f5b64-9300-4be4-94f3-455efa5c0358,536c8ca0-2c1d-49ae-9eed-1c5d068c0088,f8afa979-ef83-4ca6-84fa-a74b57481e3b,f313e93d-e100-4349-8328-f4e984062493"
+  space_id = "dd43c8b4-1ae4-4ef3-84b9-2dbfa7b7b009"
   role     = var.netdata_role
 }
