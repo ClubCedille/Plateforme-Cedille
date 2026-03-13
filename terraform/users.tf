96c8647b0c74690197fe0956694444aef80a72b2
installer <installer@gmail.com>
2026-03-13 16:10:56 -0400

    Removing greatsymphonia to add them again.

--- terraform/users.tf
+++ terraform/users.tf
@@ -355,7 +355,7 @@ module "Kyohkotsu" {
   github_username = "Kyohkotsu"
   github_role     = "member"
   teams = [
-    { teamName = "members", teamRole = "member" }, 
+    { teamName = "members", teamRole = "member" },
   ]
   cluster_name = var.cluster_name
   cluster_role = "Reader"
@@ -371,7 +371,7 @@ module "Stoaties" {
   github_username = "Stoaties"
   github_role     = "member"
   teams = [
-    { teamName = "members", teamRole = "member" }, 
+    { teamName = "members", teamRole = "member" },
   ]
   cluster_name = var.cluster_name
   cluster_role = "Reader"
@@ -386,7 +386,7 @@ module "AppleComputer381" {
   github_username = "AppleComputer381"
   github_role     = "member"
   teams = [
-    { teamName = "members", teamRole = "member" }, 
+    { teamName = "members", teamRole = "member" },
   ]
   cluster_name = var.cluster_name
   cluster_role = "Reader"
@@ -394,15 +394,15 @@ module "AppleComputer381" {
   netdata_space_id = var.netdata_space_id
   netdata_role = "observer"
 }
-  
-  
+
+
 module "SamGame" {
   source          = "./modules/user"
   github_email    = "samuel.bond.pro@gmail.com"
   github_username = "SamGame"
   github_role     = "member"
   teams = [
-    { teamName = "members", teamRole = "member" }, 
+    { teamName = "members", teamRole = "member" },
   ]
   cluster_name = var.cluster_name
   cluster_role = "Reader"
@@ -418,7 +418,7 @@ module "Meddad-Red" {
   github_username = "Meddad-Red"
   github_role     = "member"
   teams = [
-    { teamName = "members", teamRole = "member" }, 
+    { teamName = "members", teamRole = "member" },
   ]
   cluster_name = var.cluster_name
   cluster_role = "Reader"
@@ -434,7 +434,7 @@ module "Vianpyro" {
   github_username = "Vianpyro"
   github_role     = "member"
   teams = [
-    { teamName = "members", teamRole = "member" }, 
+    { teamName = "members", teamRole = "member" },
   ]
   cluster_name = var.cluster_name
   cluster_role = "Reader"
@@ -449,7 +449,7 @@ module "jonismo" {
   github_username = "jonismo"
   github_role     = "member"
   teams = [
-    { teamName = "members", teamRole = "member" }, 
+    { teamName = "members", teamRole = "member" },
   ]
   cluster_name = var.cluster_name
   cluster_role = "Reader"
@@ -465,7 +465,7 @@ module "ariannelafraise" {
   github_username = "ariannelafraise"
   github_role     = "member"
   teams = [
-    { teamName = "members", teamRole = "member" }, 
+    { teamName = "members", teamRole = "member" },
   ]
   cluster_name = var.cluster_name
   cluster_role = "Reader"
