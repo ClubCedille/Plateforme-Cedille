95cd68b39737694e9b3e151735987b305dd24f5b
Alex Vegas <v34l@proton.me>
2026-09-07 17:50:02 -0400

    tf github versions

--- terraform/modules/user/providers.tf
+++ terraform/modules/user/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     github = {
       source = "integrations/github"
-      version = "5.42.0"
+      version = "6.13.0"
     }
     netdata = {
       source = "netdata/netdata"
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,7 +12,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "~> 5.0"
+      version = "~> 6.13"
     }
     netdata = {
       source = "netdata/netdata"
