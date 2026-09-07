8e70dc23c97ece059e6071a8fffbf3734348a91f
Alex Vegas <v34l@proton.me>
2026-09-07 18:28:11 -0400

    bump provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,7 +12,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "~> 5.42"
+      version = "~> 6.13"
     }
     netdata = {
       source = "netdata/netdata"
