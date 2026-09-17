ead7319870dfbc3e469104be5e51940a0c256775
alexvegas22 <alexvegas22@users.noreply.github.com>
2026-09-17 01:46:39 +0000

    [create-pull-request] automated change

--- terraform/users.tf
+++ terraform/users.tf
@@ -537,3 +537,21 @@ module "zouud" {
   netdata_space_id = var.netdata_space_id
   netdata_role = "observer"
 }
+
+module "test-man" {
+  source          = "./modules/user"
+  github_email    = "email@mail.com"
+  github_username = "test-man"
+  github_role     = "member"
+  teams = [
+    { teamName = "members", teamRole = "member" },
+    
+    
+    
+  ]
+  cluster_name = var.cluster_name
+  cluster_role = "Operator"
+  cluster_repo = var.platform_repo
+  netdata_space_id = var.netdata_space_id
+  netdata_role = "observer"
+}
